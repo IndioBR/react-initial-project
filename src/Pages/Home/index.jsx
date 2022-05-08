5a18ba30c2edce42e0aa48b11266acbb5b863cb8
Jonathan Ximenez <ximenezjonatas@gmail.com>
2022-05-08 17:53:50 -0400

    Add some Hooks in project

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import './styles.css';
 
@@ -7,89 +7,84 @@ import { Posts } from '../../components/Posts'
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
-  state = {
+
+export const Home = () => {
+  /*state = {
     posts: [],
     allPosts: [],
     page: 0,
     postsPerPage: 10,
     searchValue: ''
-  }
+  }*/
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? allPosts.filter((post) => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos
-    })
-  }
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos)
+  }, [])
 
-  loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage)
+  }, [handleLoadPosts, postsPerPage])
+
+  const loadMorePosts = () => {
 
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
     posts.push(...nextPosts)
 
-    this.setState({ posts, page: nextPage })
+    setPosts(posts);
+    setPage(nextPage);
   }
 
-  handleSearch = (e) => {
+  const handleSearch = (e) => {
     const { value } = e.target;
-    this.setState({ ...this.state, searchValue: value })
+    setSearchValue(value)
   }
 
 
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
+  return (
+    <section className='container'>
+      {!!searchValue && (
+        <h1>Search value: {searchValue}</h1>
 
-    const filteredPosts = !!searchValue ? allPosts.filter((post) => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
-
-
-    return (
-      <section className='container'>
-        {!!searchValue && (
-          <h1>Search value: {searchValue}</h1>
-
-        )}
-        <TextInput
-          searchValue={searchValue}
-          onChange={this.handleSearch}
+      )}
+      <TextInput
+        searchValue={searchValue}
+        onChange={handleSearch}
+      />
+      {filteredPosts.length > 0 && (
+        <Posts
+          posts={filteredPosts}
         />
-        {filteredPosts.length > 0 && (
-          <Posts
-            posts={filteredPosts}
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>Não existem Posts para essa pesquisa!</p>
+      )}
+      <div className='button-container'>
+        {!searchValue && (
+          <Button
+            disabled={noMorePosts}
+            text='Load More Posts'
+            onClick={loadMorePosts}
           />
         )}
-
-        {filteredPosts.length === 0 && (
-          <p>Não existem Posts para essa pesquisa!</p>
-        )}
-        <div className='button-container'>
-          {!searchValue && (
-            <Button
-              disabled={noMorePosts}
-              text='Load More Posts'
-              onClick={this.loadMorePosts}
-            />
-          )}
-
-        </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  )
 }
 
 export default Home;
